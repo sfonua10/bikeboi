42f812fbecdf1fdea676242b440c505d350523f9
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2020-03-31 21:29:25 -0600

    changed meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `SVG animation`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@saiafonua`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
